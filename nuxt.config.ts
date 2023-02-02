a3ddac491b3ddc714f7949b6f39120f15a73e296
Kevin Kretz <68304024+theKevinKretz@users.noreply.github.com>
2023-02-02 11:11:16 +0100

    Support for custom domain

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: ["@nuxtjs/tailwindcss"],
-    app: {
-        baseURL: '/personal-website/' // baseURL: '/<repository>/'
-    }
+    // app: {
+    //     baseURL: '/personal-website/' // baseURL: '/<repository>/'
+    // }
 })
